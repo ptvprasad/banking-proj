1009424a582a05a74949cf0e9182db3d7ad22a3a
ptvprasad <163627197+ptvprasad@users.noreply.github.com>
2024-10-23 02:51:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ data "aws_security_group" "proj_sg" {
   }
 }
 
-resource "aws_instance" "prodc"{
+resource "aws_instance" "prodc" {
   ami             = "ami-0e86e20dae9224db8"
   key_name        = "jendock"
   security_groups = [data.aws_security_group.proj_sg.id]
@@ -16,7 +16,7 @@ resource "aws_instance" "prodc"{
   }
 }
 
-resource "aws_instance" "monitorc"{
+resource "aws_instance" "monitorc" {
   ami             = "ami-0e86e20dae9224db8"
   key_name        = "host"
   security_groups = [data.aws_security_group.proj_sg.id]
