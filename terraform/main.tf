c29ba6aa9fc1d630aa36b2c9a7e095451cb4d0af
ptvprasad <163627197+ptvprasad@users.noreply.github.com>
2024-10-23 12:52:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,21 @@
-data "aws_security_group" "proj_sg" {      #using existing sec groups
-  filter {
-    name   = "group-name"
-    values = ["proj-sg"]
+resource "aws_instance" "production" {
+  ami                    = "ami-0e86e20dae9224db8"
+  key_name               = "asg"
+  vpc_security_group_ids = ["sg-0fa119bc6bab10ff8"]  # Directly use the security group ID
+  instance_type          = "t2.micro"
+  count                  = 1
+  tags = {
+    Name = "production"
   }
 }
 
-resource "aws_instance" "prodc" {
-  ami             = "ami-0e86e20dae9224db8"
-  key_name        = "jendock"
-  vpc_security_group_ids = [data.aws_security_group.proj_sg.id]  # Use the ID from data block
-  instance_type   = "t2.micro"
-  count           = 1
+resource "aws_instance" "monitor" {
+  ami                    = "ami-0e86e20dae9224db8"
+  key_name               = "asg"
+  vpc_security_group_ids = ["sg-0fa119bc6bab10ff8"]  # Directly use the security group ID
+  instance_type          = "t2.micro"
+  count                  = 1
   tags = {
-    Name = "prodk"
-  }
-}
-
-resource "aws_instance" "monitorc" {
-  ami             = "ami-0e86e20dae9224db8"
-  key_name        = "host"
-  vpc_security_group_ids = [data.aws_security_group.proj_sg.id]  # Use the ID from data block
-  instance_type   = "t2.micro"
-  count           = 1
-  tags = {
-    Name = "monitorchk"
+    Name = "monitor"
   }
 }
